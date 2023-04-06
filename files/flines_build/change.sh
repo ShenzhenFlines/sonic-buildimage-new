92675b69b593a7ffdd21f0102b6bd169632aebb7
Wei Cao <wei.cao@phoenixcompany.cn>
2023-04-06 14:25:25 +0800

    feat: add change.sh for increasement build

--- files/flines_build/change.sh
+++ files/flines_build/change.sh
@@ -1,14 +1,19 @@
-﻿# !/bin/bash
+﻿#!/bin/bash
 # work directory must be project root directory, not this shell directory
-# call me use ./files/flines_build/start.sh
+# call me use ./files/flines_build/change.sh
 
 if [ ! -f Makefile.work ]
 then
     echo run me at git repository root directory
     exit -2
 fi
-
 export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
+
+git reset --hard HEAD
+git checkout -f .
+git checkout evb_8t_rel
+git pull -fr 
+git log -n 1
 
 cp -f files/flines_build/.gitmodules ./
 cp -f files/flines_build/Makefile.work ./
@@ -19,14 +24,8 @@ cp -f files/flines_build/docker-base-buster/versions-deb-buster files/build/vers
 cp -f files/flines_build/dockers/docker-base/Dockerfile.j2 dockers/docker-base-stretch/Dockerfile.j2
 cp -f files/flines_build/dockers/docker-base-buster/Dockerfile.j2 dockers/docker-base-buster/Dockerfile.j2
 
-make init
-cd src/sonic-swss
-git am ../../platform/clounix/patch/0001-support-evb8t-for-sonic-swss.patch
-cd ../sonic-utilities/
-git am ../../platform/clounix/patch/0001-support-evb8t-for-sonic-utilities.patch
-cd ../sonic-sairedis
-git am ../../platform/clounix/patch/0001-support-evb8t-for-sonic-sairedis.patch
-cd ../../
-make configure PLATFORM=clounix
+rm target/debs/buster/sonic-device-data* || true
+rm target/sonic-clounix.bin* || true
+rm target/debs/buster/sonic-platform-flines* || true
 make target/sonic-clounix.bin
 # cppcheck --enable=all --xml --xml-version=2.0 platform/clounix/sonic-platform-modules-flines >out.xml 2>err.xml 
