4d017ac287464301ff450dce1350e2594ad76a29
Wei Cao <wei.cao@phoenixcompany.cn>
2023-04-06 15:47:00 +0800

    fix: copy dockerfiles failed because of error file names

--- files/flines_build/change.sh
+++ files/flines_build/change.sh
@@ -9,23 +9,28 @@ then
 fi
 export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
 
-git reset --hard HEAD
-git checkout -f .
-git checkout evb_8t_rel
-git pull -fr 
+# do not uncomment follwing lines during self test
+# git reset --hard HEAD
+# git checkout -f .
+# git checkout evb_8t_rel
+# git pull -fr 
 git log -n 1
 
 cp -f files/flines_build/.gitmodules ./
 cp -f files/flines_build/Makefile.work ./
-cp -f files/flines_build/Dockfile* sonic-slave-buster/
-cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
+# cp -f files/flines_build/sonic-slave-buster/* sonic-slave-buster/
+# cp -f files/flines_build/sonic-slave-stretch/* sonic-slave-stretch/
+# cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
 cp -f files/flines_build/docker-base-stretch/versions-deb-stretch files/build/versions/dockers/docker-base-stretch/versions-deb-stretch
 cp -f files/flines_build/docker-base-buster/versions-deb-buster files/build/versions/dockers/docker-base-buster/versions-deb-buster
-cp -f files/flines_build/dockers/docker-base/Dockerfile.j2 dockers/docker-base-stretch/Dockerfile.j2
+cp -f files/flines_build/dockers/docker-base/Dockerfile.j2 dockers/docker-base/Dockerfile.j2
 cp -f files/flines_build/dockers/docker-base-buster/Dockerfile.j2 dockers/docker-base-buster/Dockerfile.j2
 
 rm target/debs/buster/sonic-device-data* || true
 rm target/sonic-clounix.bin* || true
 rm target/debs/buster/sonic-platform-flines* || true
-make target/sonic-clounix.bin
+# make target/sonic-clounix.bin
+nohup make target/sonic-clounix.bin >out.log 2>err.log &
+sleep 2
+tail -f out.log
 # cppcheck --enable=all --xml --xml-version=2.0 platform/clounix/sonic-platform-modules-flines >out.xml 2>err.xml 
--- files/flines_build/start.sh
+++ files/flines_build/start.sh
@@ -12,11 +12,11 @@ export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
 
 cp -f files/flines_build/.gitmodules ./
 cp -f files/flines_build/Makefile.work ./
-cp -f files/flines_build/Dockfile* sonic-slave-buster/
+cp -f files/flines_build/Dockerfile* sonic-slave-buster/
 cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
 cp -f files/flines_build/docker-base-stretch/versions-deb-stretch files/build/versions/dockers/docker-base-stretch/versions-deb-stretch
 cp -f files/flines_build/docker-base-buster/versions-deb-buster files/build/versions/dockers/docker-base-buster/versions-deb-buster
-cp -f files/flines_build/dockers/docker-base/Dockerfile.j2 dockers/docker-base-stretch/Dockerfile.j2
+cp -f files/flines_build/dockers/docker-base/Dockerfile.j2 dockers/docker-base/Dockerfile.j2
 cp -f files/flines_build/dockers/docker-base-buster/Dockerfile.j2 dockers/docker-base-buster/Dockerfile.j2
 
 make init
