c7a91c0d779cec1de89386c51c4c5e75560f83f6
Wei Cao <wei.cao@phoenixcompany.cn>
2023-04-11 20:40:57 +0800

    fix: Set SONIC_IMAGE_VERSION by jenkins, not shell

--- files/flines_build/change.sh
+++ files/flines_build/change.sh
@@ -7,13 +7,15 @@ then
     echo run me at git repository root directory
     exit -2
 fi
-export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
+
+# Set by jenkins
+# export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
 
 # do not uncomment follwing lines during self test
-# git reset --hard HEAD
-# git checkout -f .
-# git checkout evb_8t_rel
-# git pull -fr 
+git reset --hard HEAD
+git checkout -f .
+git checkout evb_8t_rel
+git pull -fr 
 git log -n 1
 
 cp -f files/flines_build/.gitmodules ./
@@ -21,6 +23,7 @@ cp -f files/flines_build/Makefile.work ./
 # cp -f files/flines_build/sonic-slave-buster/* sonic-slave-buster/
 # cp -f files/flines_build/sonic-slave-stretch/* sonic-slave-stretch/
 # cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
+cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
 cp -f files/flines_build/docker-base-stretch/versions-deb-stretch files/build/versions/dockers/docker-base-stretch/versions-deb-stretch
 cp -f files/flines_build/docker-base-buster/versions-deb-buster files/build/versions/dockers/docker-base-buster/versions-deb-buster
 cp -f files/flines_build/dockers/docker-base/Dockerfile.j2 dockers/docker-base/Dockerfile.j2
@@ -29,8 +32,8 @@ cp -f files/flines_build/dockers/docker-base-buster/Dockerfile.j2 dockers/docker
 rm target/debs/buster/sonic-device-data* || true
 rm target/sonic-clounix.bin* || true
 rm target/debs/buster/sonic-platform-flines* || true
-# make target/sonic-clounix.bin
-nohup make target/sonic-clounix.bin >out.log 2>err.log &
-sleep 2
-tail -f out.log
+make target/sonic-clounix.bin
+# nohup make target/sonic-clounix.bin >out.log 2>err.log &
+# sleep 2
+#tail -f out.log
 # cppcheck --enable=all --xml --xml-version=2.0 platform/clounix/sonic-platform-modules-flines >out.xml 2>err.xml 
--- files/flines_build/start.sh
+++ files/flines_build/start.sh
@@ -8,11 +8,14 @@ then
     exit -2
 fi
 
-export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
+# Set by jenkins
+# export SONIC_IMAGE_VERSION=$(date +%Y%m%d%H%M)
 
 cp -f files/flines_build/.gitmodules ./
 cp -f files/flines_build/Makefile.work ./
-cp -f files/flines_build/Dockerfile* sonic-slave-buster/
+# cp -f files/flines_build/sonic-slave-buster/* sonic-slave-buster/
+# cp -f files/flines_build/sonic-slave-stretch/* sonic-slave-stretch/
+# cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
 cp -f files/flines_build/sources.list.amd64 files/apt/sources.list.amd64
 cp -f files/flines_build/docker-base-stretch/versions-deb-stretch files/build/versions/dockers/docker-base-stretch/versions-deb-stretch
 cp -f files/flines_build/docker-base-buster/versions-deb-buster files/build/versions/dockers/docker-base-buster/versions-deb-buster
