13e6b6b9b9afc178c9bffd9beb83bf36ba949a38
Wei Cao <wei.cao@phoenixcompany.cn>
2023-07-25 17:24:57 +0800

    产测脚本文件打包至/var/vtysh

--- build_debian.sh
+++ build_debian.sh
@@ -80,8 +80,12 @@ if [ ! -d $FILESYSTEM_ROOT/usr/bin ]; then
     sudo chown -R root:root $FILESYSTEM_ROOT/usr/bin
 fi
 sudo cp -f files/vtysh_bin/vtysh $FILESYSTEM_ROOT/usr/local/bin/
-sudo cp -f files/vtysh_bin/all.sh $FILESYSTEM_ROOT/home/admin
-sudo cp -f files/vtysh_bin/all_other.sh $FILESYSTEM_ROOT/home/admin
+if [ ! -d $FILESYSTEM_ROOT/var/vtysh ]; then
+    sudo mkdir -p $FILESYSTEM_ROOT/var/vtysh
+    sudo chown -R root:root $FILESYSTEM_ROOT/var/vtysh
+fi
+sudo cp -f files/vtysh_bin/all.sh $FILESYSTEM_ROOT/var/vtysh
+sudo cp -f files/vtysh_bin/all_other.sh $FILESYSTEM_ROOT/var/vtysh
 sudo cp -f files/clounix_bin/clx_fpga $FILESYSTEM_ROOT/usr/bin/
 sudo cp -f files/clounix_bin/cpld_update_app $FILESYSTEM_ROOT/usr/bin/
 sudo chown root:root $FILESYSTEM_ROOT/usr/local/bin/vtysh
