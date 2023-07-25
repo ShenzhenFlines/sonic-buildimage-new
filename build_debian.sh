80d31fbd7c1cbe395bafe087749350ee649a7edd
Wei Cao <wei.cao@phoenixcompany.cn>
2023-07-25 15:59:07 +0800

    vtysh关联脚本放到/home/admin

--- build_debian.sh
+++ build_debian.sh
@@ -80,7 +80,8 @@ if [ ! -d $FILESYSTEM_ROOT/usr/bin ]; then
     sudo chown -R root:root $FILESYSTEM_ROOT/usr/bin
 fi
 sudo cp -f files/vtysh_bin/vtysh $FILESYSTEM_ROOT/usr/local/bin/
-sudo cp -f files/vtysh_bin/all.sh $FILESYSTEM_ROOT/usr/local/bin/
+sudo cp -f files/vtysh_bin/all.sh $FILESYSTEM_ROOT/home/admin
+sudo cp -f files/vtysh_bin/all_other.sh $FILESYSTEM_ROOT/home/admin
 sudo cp -f files/clounix_bin/clx_fpga $FILESYSTEM_ROOT/usr/bin/
 sudo cp -f files/clounix_bin/cpld_update_app $FILESYSTEM_ROOT/usr/bin/
 sudo chown root:root $FILESYSTEM_ROOT/usr/local/bin/vtysh
